49362b93fca25cbc65da3c3307ece9ce3c04b4f3
Argemenon <sriharshmishrapalkot@gmail.com>
2024-12-24 22:10:29 +0530

    arithmetic operations

--- Image Processing/rough.py
+++ Image Processing/rough.py
@@ -1,9 +1,10 @@
 #Import only if not previously imported
 import cv2
-VariableName = cv2.imread("Image Processing/Images/white.jpg",-1)     #(flag = 0 or 1 or -1)
+import matplotlib.pyplot as plt
+import numpy
 
-#Import only if not previously imported
+img = cv2.imread('Image Processing/Images/bwdiagonal.jpg', -1)
 
-cv2.imshow("White", VariableName)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.imshow(img)
+
+plt.show()
