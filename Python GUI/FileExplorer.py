fa9e8635aba246653442c9cbd26291d9dd4da8d4
Argemenon <sriharshmishrapalkot@gmail.com>
2025-01-08 18:35:08 +0530

    code cleanup

--- Python GUI/FileExplorer.py
+++ Python GUI/FileExplorer.py
@@ -3,34 +3,48 @@ import ttkbootstrap as ttk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-def OpenFile():
-    out = filedialog.askopenfilename()
-    pic = Image.open(out)
-    a,b = pic.size
-    ratio = b/a
-    pic_resized = pic.resize((250,int(250*(ratio))), Image.Resampling.NEAREST)
-    new_pic = ImageTk.PhotoImage(pic_resized)
-    img_label.configure(image= new_pic)
-    img_label.image = new_pic
+#definitions
+def OpenFile(): #opens the file and resizes it
+    directory = filedialog.askopenfilename(
+        initialdir= "C:\\Users\\DELL\\OneDrive\\Pictures\\POSTERS", 
+        filetypes= (("jpeg files", "*.jpg"),("png files", "*.png"))
+        )
+    pic = Image.open(directory)
+    h,w = pic.size
+    ratio = w/h
+    pic_resized = pic.resize((450,int(450*(ratio))), Image.Resampling.NEAREST)
+    resized_pic = ImageTk.PhotoImage(pic_resized)
+    img_label.configure(image= resized_pic)
+    img_label.image = resized_pic
 
-def exit_func(event):
+def exit_func(event): #closes the window when exit is selected
     v = list_var.get()
     if v == item[1]:
         exit()
-    
-window = ttk.Window()
-window.title('File Explorer')
-window.geometry('550x600')
 
-button = ttk.Button(window, text= 'Open File Explorer', command= OpenFile)
-button.pack()
+#window    
+window = ttk.Window(themename= 'solar')
+window.title('Image Displayer')
+window.geometry('550x700')
 
-exit_button = ttk.Button(window, text= 'Exit', command= lambda: exit())
-exit_button.pack(pady= 10)
+#button frame starts
+frame = ttk.Frame()
 
+#buttons
+open_button = ttk.Button(frame, text= 'Open File Explorer', command= OpenFile)
+open_button.pack(pady= 5, padx= 3)
+open_button.pack(side= 'left')
+
+exit_button = ttk.Button(frame, text= 'Exit', command= lambda: exit())
+exit_button.pack(side= 'left')
+
+frame.pack() #button frame ends
+
+#image
 img_label = ttk.Label(window)
 img_label.pack(pady= 10)
 
+#combobox
 item = ('Do Nothing', 'Exit')
 list_var = tk.StringVar(value = item[0])
 combobox = ttk.Combobox(window, textvariable= list_var)
@@ -38,5 +52,5 @@ combobox['values'] = item
 combobox.bind('<FocusIn>', exit_func)
 combobox.pack()
 
-
+#run
 window.mainloop()
